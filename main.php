35621db39ca6db199483c6627a472b0928b56c5a
allexyy <38141007+allexyy@users.noreply.github.com>
2020-01-13 01:32:43 +0300

    Oxem

--- main.php
+++ main.php
@@ -1,20 +1,39 @@
-<?php 
-class Hlev{
-    public function getEat($animal, $min, $max)//Сбор продукта у животных (Т.к все они имеют уникальный номер пробегаемся по их общему кол-ву без страха перепутать)
+<?php
+//Класс описывающий общие парамтры животных
+class Animal {
+    public $count;
+    public $prod;
+    public $prodname;
+}
+class Cow extends Animal{
+    //Задаем нужные параметры при вызове класса
+    function __construct()
     {
-        $prod=0;
-        for($i=0;$i<=$animal;$i++){
-            $prod += rand($min, $max);
-        }
-        return $prod;
+        $this->count = 10;
+        $this->prod = rand(8,12);
+        $this->prodname = " литров молока";
     }
 }
-$hlev = new Hlev();
-//Заводим животных в хлев
-$hlev->cow = 10;
-$hlev->chicken = 20;
-//Доим коров, собираем яйца
-$milk = $hlev->getEat($hlev->cow,8,12);
-$egg= $hlev->getEat($hlev->chicken,0,1);
-//Выводим на экран общее кол-во собранной продукции
-echo "Удалось собрать: $milk литров молока и $egg яиц";
+class Chicken extends Animal{
+    function __construct()
+    {
+        $this->count = 20;
+        $this->prod = rand(0,1);
+        $this->prodname = " яиц";
+    }
+}
+class Hlev{
+    //Метод сбора продукции
+    public function getEat($count, $prod){
+        $fullprod = 0;
+        for($i=0;$i<$count;$i++){
+            $fullprod += $prod;
+        }
+        return $fullprod;
+    }
+}
+$hlev = new Hlev;
+$cow = new Cow;
+$chicken = new Chicken;
+echo $hlev->getEat($cow->count,$cow->prod).$cow->prodname;
+echo ' и '.$hlev->getEat($chicken->count,$chicken->prod).$chicken->prodname;
